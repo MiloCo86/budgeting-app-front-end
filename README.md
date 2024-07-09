23a83dc424ce8a2a042799618a86822f274cac53
MiloCo86 <cpaez@pursuit.org>
2024-07-09 18:32:03 -0400

    README file updated

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
-# React + Vite
+# Bugdeg App!
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+use https://github.com/MiloCo86/budgeting-app-back-end as a back-end
 
-Currently, two official plugins are available:
+### Description:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+The Budget App is your go-to solution for managing and keeping track of all your transactions efficiently. the app allows you to:
+
+![home](/src/assets/screenshots/home.png)
+
+#### Summary: 
+Get a clear overview of all your transactions in one place. The summary provides a the total amount and the number of transactions.
+![summary](/src/assets/screenshots/summary.png)
+
+#### Transactions: 
+Show all the transactions with the description, date and the amount.
+![transactions](/src/assets/screenshots/transactions.png)
+
+#### New Transactions: 
+Easily add new transactions
+![newTransaction](/src/assets/screenshots/newTransaction.png)
+
+#### Show Transaction: 
+Click in a transaction to see a full detail card with all the elements of the transaction
+![transaction](/src/assets/screenshots/transaction.png)
+
+#### Edit or Delete Transactions: 
+Allows you to  make changes to an existing transaction
+![editTransaction](/src/assets/screenshots/editTransaction.png)
+
+
+
+
+
+
--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -8,25 +8,54 @@ import newIcon from '../assets/new.svg'
 
 
 const Transactions = () => {
-    const [transactions, setTransactions] = useState([])
-    const API = import.meta.env.VITE_BASE_URL
+  const [transactions, setTransactions] = useState([])
+  const API = import.meta.env.VITE_BASE_URL
 
-    useEffect(()=>{
-        fetch(API)
-            .then(res => res.json())
-            .then(res => {
-              setTransactions(res)
-            })
-            .catch(err => console.error(err))
-    },[])
+  useEffect(()=>{
+      fetch(API)
+          .then(res => res.json())
+          .then(res => {
+            setTransactions(res)
+          })
+          .catch(err => console.error(err))
+  },[])
+
+  const handleSort = (e)=>{
+    const sortTransactions = [...transactions]
+
+    switch(e.target.value){
+      case "low":
+        setTransactions(sortTransactions.sort((a,b)=>a.amount -b.amount))
+        break;
+      case "high":
+        setTransactions(sortTransactions.sort((a,b)=>b.amount -a.amount))
+        break;
+      case "name":
+        setTransactions(sortTransactions.sort((a,b)=>{
+          if(a.description < b.description) { return -1; }
+          if(a.description > b.description) { return 1; }
+          return 0;
+        }))
+        break;      
+    }
+   
+    
+  }
 
   return (
     <div className='transactions-container'>
       <h3 className="transactions-title">Transactions</h3>
-      
-      <Link className='newicon' to={'/transactions/new'}>
-        <img src={newIcon} alt="New Transaction" />
-      </Link>
+      <div className="sortAndNew">
+        <select className='sort' name="sort" id="sort" onChange={handleSort}>
+          <option value="" hidden></option>
+          <option value="low">Price (low to high)</option>
+          <option value="high">Price (high to low)</option>
+          <option value="name">Name (A to Z)</option>
+        </select>
+        <Link className='newicon' to={'/transactions/new'}>
+          <img src={newIcon} alt="New Transaction" />
+        </Link>
+      </div>
       <div className='transactions-list'>
         { transactions.map((transaction, idx) =>{
           return (
